0ea7c0b8f700fc601e128c76a619755eecabc34c
Gavin <gav.ohara@gmail.com>
2021-01-20 10:15:30 +0000

    "Initial Commit"

---
+++ GBlog/Models/Blog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GBlog.Models
+{
+    public class Blog
+    {
+        public virtual int BlogId { get; set; }
+        
+        [Required]
+        public virtual string Title { get; set; }
+        
+        [AllowHtml]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public virtual string Content { get; set; }
+
+        public virtual DateTime CreatedOn { get; set; }
+        
+        public virtual DateTime ChangedOn { get; set; }
+    }
+}
+
